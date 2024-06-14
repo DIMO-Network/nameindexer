b38ca60b270ef4a70febdb045d127e636aad1e66
Kevin Joiner <10265309+KevinJoiner@users.noreply.github.com>
2024-06-14 13:51:03 -0400

    Add support for using tokendId instead of address.

--- index_test.go
+++ index_test.go
@@ -18,10 +18,12 @@ func TestEncodeIndex(t *testing.T) {
 		{
 			name: "Valid index encoding with default fillers",
 			input: &Index{
-				Timestamp:       time.Date(2024, 6, 11, 15, 30, 0, 0, time.UTC),
-				PrimaryFiller:   "",
-				DataType:        "Stat/2.0.0",
-				Address:         common.HexToAddress("0xc57d6d57fca59d0517038c968a1b831b071fa679"),
+				Timestamp:     time.Date(2024, 6, 11, 15, 30, 0, 0, time.UTC),
+				PrimaryFiller: "",
+				DataType:      "Stat/2.0.0",
+				Subject: Subject{
+					Address: ref(common.HexToAddress("0xc57d6d57fca59d0517038c968a1b831b071fa679")),
+				},
 				SecondaryFiller: "",
 			},
 			expected:  "759388MMStat/2.0.0C57D6D57fcA59d0517038c968A1b831B071FA67900153000",
@@ -30,22 +32,55 @@ func TestEncodeIndex(t *testing.T) {
 		{
 			name: "Valid index encoding with custom fillers",
 			input: &Index{
-				Timestamp:       time.Date(2024, 6, 11, 15, 30, 0, 0, time.UTC),
-				PrimaryFiller:   "XX",
-				DataType:        "Stat/2.0.0",
-				Address:         common.HexToAddress("0xc57d6d57fca59d0517038c968a1b831b071fa679"),
+				Timestamp:     time.Date(2024, 6, 11, 15, 30, 0, 0, time.UTC),
+				PrimaryFiller: "XX",
+				DataType:      "Stat/2.0.0",
+				Subject: Subject{
+					Address: ref(common.HexToAddress("0xc57d6d57fca59d0517038c968a1b831b071fa679")),
+				},
 				SecondaryFiller: "99",
 			},
 			expected:  "759388XXStat/2.0.0C57D6D57fcA59d0517038c968A1b831B071FA67999153000",
 			expectErr: false,
 		},
 		{
+			name: "Valid index encoding with TokenID",
+			input: &Index{
+				Timestamp:     time.Date(2024, 6, 11, 15, 30, 0, 0, time.UTC),
+				PrimaryFiller: "MM",
+				DataType:      "Stat/2.0.0",
+				Subject: Subject{
+					TokenID: ref(uint32(1234567890)),
+				},
+				SecondaryFiller: "00",
+			},
+			expected:  "759388MMStat/2.0.0T00000000000000000000000000000123456789000153000",
+			expectErr: false,
+		},
+		{
+			name: "Valid index encoding with TokenID && Address",
+			input: &Index{
+				Timestamp:     time.Date(2024, 6, 11, 15, 30, 0, 0, time.UTC),
+				PrimaryFiller: "MM",
+				DataType:      "Stat/2.0.0",
+				Subject: Subject{
+					Address: ref(common.HexToAddress("0xc57d6d57fca59d0517038c968a1b831b071fa679")),
+					TokenID: ref(uint32(1234567890)),
+				},
+				SecondaryFiller: "00",
+			},
+			expected:  "759388MMStat/2.0.0C57D6D57fcA59d0517038c968A1b831B071FA67900153000",
+			expectErr: false,
+		},
+		{
 			name: "Invalid primary filler length",
 			input: &Index{
-				Timestamp:       time.Date(2024, 6, 11, 15, 30, 0, 0, time.UTC),
-				PrimaryFiller:   "MMM",
-				DataType:        "Stat/2.0.0",
-				Address:         common.HexToAddress("0xc57d6d57fca59d0517038c968a1b831b071fa679"),
+				Timestamp:     time.Date(2024, 6, 11, 15, 30, 0, 0, time.UTC),
+				PrimaryFiller: "MMM",
+				DataType:      "Stat/2.0.0",
+				Subject: Subject{
+					Address: ref(common.HexToAddress("0xc57d6d57fca59d0517038c968a1b831b071fa679")),
+				},
 				SecondaryFiller: "00",
 			},
 			expected:  "",
@@ -54,10 +89,12 @@ func TestEncodeIndex(t *testing.T) {
 		{
 			name: "Invalid secondary filler length",
 			input: &Index{
-				Timestamp:       time.Date(2024, 6, 11, 15, 30, 0, 0, time.UTC),
-				PrimaryFiller:   "MM",
-				DataType:        "Stat/2.0.0",
-				Address:         common.HexToAddress("0xc57d6d57fca59d0517038c968a1b831b071fa679"),
+				Timestamp:     time.Date(2024, 6, 11, 15, 30, 0, 0, time.UTC),
+				PrimaryFiller: "MM",
+				DataType:      "Stat/2.0.0",
+				Subject: Subject{
+					Address: ref(common.HexToAddress("0xc57d6d57fca59d0517038c968a1b831b071fa679")),
+				},
 				SecondaryFiller: "000",
 			},
 			expected:  "",
@@ -66,10 +103,12 @@ func TestEncodeIndex(t *testing.T) {
 		{
 			name: "DataType needs padding",
 			input: &Index{
-				Timestamp:       time.Date(2020, 6, 2, 15, 30, 0, 0, time.UTC),
-				PrimaryFiller:   "MM",
-				DataType:        "Stat",
-				Address:         common.HexToAddress("0xc57d6d57fca59d0517038c968a1b831b071fa679"),
+				Timestamp:     time.Date(2020, 6, 2, 15, 30, 0, 0, time.UTC),
+				PrimaryFiller: "MM",
+				DataType:      "Stat",
+				Subject: Subject{
+					Address: ref(common.HexToAddress("0xc57d6d57fca59d0517038c968a1b831b071fa679")),
+				},
 				SecondaryFiller: "00",
 			},
 			expected:  "799397MM000000StatC57D6D57fcA59d0517038c968A1b831B071FA67900153000",
@@ -78,10 +117,12 @@ func TestEncodeIndex(t *testing.T) {
 		{
 			name: "DataType too long",
 			input: &Index{
-				Timestamp:       time.Date(2020, 6, 2, 15, 30, 0, 0, time.UTC),
-				PrimaryFiller:   "MM",
-				DataType:        "Stat/2.0.0.0.0.0",
-				Address:         common.HexToAddress("0xc57d6d57fca59d0517038c968a1b831b071fa679"),
+				Timestamp:     time.Date(2020, 6, 2, 15, 30, 0, 0, time.UTC),
+				PrimaryFiller: "MM",
+				DataType:      "Stat/2.0.0.0.0.0",
+				Subject: Subject{
+					Address: ref(common.HexToAddress("0xc57d6d57fca59d0517038c968a1b831b071fa679")),
+				},
 				SecondaryFiller: "00",
 			},
 			expected:  "",
@@ -90,10 +131,12 @@ func TestEncodeIndex(t *testing.T) {
 		{
 			name: "Invalid date part",
 			input: &Index{
-				Timestamp:       time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC),
-				PrimaryFiller:   "MM",
-				DataType:        "Stat",
-				Address:         common.HexToAddress("0xc57d6d57fca59d0517038c968a1b831b071fa679"),
+				Timestamp:     time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC),
+				PrimaryFiller: "MM",
+				DataType:      "Stat",
+				Subject: Subject{
+					Address: ref(common.HexToAddress("0xc57d6d57fca59d0517038c968a1b831b071fa679")),
+				},
 				SecondaryFiller: "00",
 			},
 			expected:  "",
@@ -131,10 +174,26 @@ func TestDecodeIndex(t *testing.T) {
 			name:  "Valid index decoding",
 			input: "759388MMStat/2.0.0c57d6d57fca59d0517038c968a1b831b071fa67900153000",
 			expected: Index{
-				Timestamp:       time.Date(2024, 6, 11, 15, 30, 0, 0, time.UTC),
-				PrimaryFiller:   "MM",
-				DataType:        "Stat/2.0.0",
-				Address:         common.HexToAddress("0xc57d6d57fca59d0517038c968a1b831b071fa679"),
+				Timestamp:     time.Date(2024, 6, 11, 15, 30, 0, 0, time.UTC),
+				PrimaryFiller: "MM",
+				DataType:      "Stat/2.0.0",
+				Subject: Subject{
+					Address: ref(common.HexToAddress("0xc57d6d57fca59d0517038c968a1b831b071fa679")),
+				},
+				SecondaryFiller: "00",
+			},
+			expectErr: false,
+		},
+		{
+			name:  "Valid index decoding with tokenId",
+			input: "759388MMStat/2.0.0T00000000000000000000000000000000092342300153000",
+			expected: Index{
+				Timestamp:     time.Date(2024, 6, 11, 15, 30, 0, 0, time.UTC),
+				PrimaryFiller: "MM",
+				DataType:      "Stat/2.0.0",
+				Subject: Subject{
+					TokenID: ref(uint32(923423)),
+				},
 				SecondaryFiller: "00",
 			},
 			expectErr: false,
@@ -149,10 +208,12 @@ func TestDecodeIndex(t *testing.T) {
 			name:  "DataType needs trimming",
 			input: "759388MM000000Statc57d6d57fca59d0517038c968a1b831b071fa67900153000",
 			expected: Index{
-				Timestamp:       time.Date(2024, 6, 11, 15, 30, 0, 0, time.UTC),
-				PrimaryFiller:   "MM",
-				DataType:        "Stat",
-				Address:         common.HexToAddress("0xc57d6d57fca59d0517038c968a1b831b071fa679"),
+				Timestamp:     time.Date(2024, 6, 11, 15, 30, 0, 0, time.UTC),
+				PrimaryFiller: "MM",
+				DataType:      "Stat",
+				Subject: Subject{
+					Address: ref(common.HexToAddress("0xc57d6d57fca59d0517038c968a1b831b071fa679")),
+				},
 				SecondaryFiller: "00",
 			},
 			expectErr: false,
@@ -194,9 +255,33 @@ func TestDecodeIndex(t *testing.T) {
 
 // compareIndices compares two Index structs.
 func compareIndices(a, b *Index) bool {
+	if a == nil || b == nil {
+		return a == b
+	}
+
+	if a.Subject.Address != nil && b.Subject.Address != nil {
+		if *a.Subject.Address != *b.Subject.Address {
+			return false
+		}
+	} else if a.Subject.TokenID == b.Subject.TokenID {
+		return false
+	}
+
+	if a.Subject.TokenID != nil && b.Subject.TokenID != nil {
+		if *a.Subject.TokenID != *b.Subject.TokenID {
+			return false
+		}
+	} else if a.Subject.TokenID != b.Subject.TokenID {
+		return false
+	}
+
 	return a.Timestamp.Equal(b.Timestamp) &&
 		a.PrimaryFiller == b.PrimaryFiller &&
 		strings.TrimSpace(a.DataType) == strings.TrimSpace(b.DataType) &&
-		a.Address == b.Address &&
 		a.SecondaryFiller == b.SecondaryFiller
+}
+
+// ref returns a reference to the input.
+func ref[T any](v T) *T {
+	return &v
 }
