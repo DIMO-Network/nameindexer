c7c3b767fa76a9fa57c45d056b895103b5913d47
Kevin Joiner <10265309+KevinJoiner@users.noreply.github.com>
2024-06-21 15:47:15 -0400

    Add service for clickhouse

--- README.md
+++ README.md
@@ -4,7 +4,26 @@
 [![GoDoc](https://godoc.org/github.com/DIMO-Network/nameindexer?status.svg)](https://godoc.org/github.com/DIMO-Network/nameindexer)
 [![Go Report Card](https://goreportcard.com/badge/github.com/DIMO-Network/nameindexer)](https://goreportcard.com/report/github.com/DIMO-Network/nameindexer)
 
-Run `make`
+This repository is responsible for creating and managing indexable names for objects.
+
+Use `make` to manage the project building, testing, and linting.
+
+```
+> make help
+
+Specify a subcommand:
+
+  build                Build the code
+  clean                Clean the project binaries
+  tidy                 tidy the go modules
+  test                 Run the all tests
+  lint                 Run the linter
+  format               Run the linter with fix
+  migration            Generate migration file specify name with name=your_migration_name
+  tools                Install all tools
+  tools-golangci-lint  Install golangci-lint
+  tools-migration      Install migration tool
+```
 
 ## License
 
---
+++ pkg/clickhouse/service/service_test.go
@@ -0,0 +1,210 @@
+package service_test
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/ClickHouse/clickhouse-go/v2"
+	chConfig "github.com/DIMO-Network/clickhouse-infra/pkg/connect/config"
+	"github.com/DIMO-Network/clickhouse-infra/pkg/container"
+	"github.com/DIMO-Network/nameindexer"
+	chindexer "github.com/DIMO-Network/nameindexer/pkg/clickhouse"
+	"github.com/DIMO-Network/nameindexer/pkg/clickhouse/migrations"
+	"github.com/DIMO-Network/nameindexer/pkg/clickhouse/service"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+)
+
+var dataType = "0123456789"
+
+// setupClickHouseContainer starts a ClickHouse container for testing and returns the connection.
+func setupClickHouseContainer(t *testing.T) *container.Container {
+	ctx := context.Background()
+	settings := chConfig.Settings{
+		User:     "default",
+		Database: "dimo",
+	}
+
+	chContainer, err := container.CreateClickHouseContainer(ctx, settings)
+	require.NoError(t, err)
+
+	chDB, err := chContainer.GetClickhouseAsDB()
+	require.NoError(t, err)
+
+	// Ensure we terminate the container at the end
+	t.Cleanup(func() {
+		chContainer.Terminate(ctx)
+	})
+
+	err = migrations.RunGoose(ctx, []string{"up"}, chDB)
+	require.NoError(t, err)
+
+	return chContainer
+}
+
+// insertTestData inserts test data into ClickHouse.
+func insertTestData(t *testing.T, ctx context.Context, conn clickhouse.Conn, subject nameindexer.Subject, filename string, timestamp time.Time) {
+	query := fmt.Sprintf("INSERT INTO %s (%s, %s, %s, %s) VALUES (?, ?, ?, ?)", chindexer.TableName,
+		chindexer.SubjectColumn, chindexer.FileNameColumn, chindexer.TimestampColumn, chindexer.DataTypeColumn)
+	err := conn.Exec(ctx, query, subject, filename, timestamp, dataType)
+	require.NoError(t, err)
+}
+
+// TestGetLatestFileName tests the GetLatestFileName function.
+func TestGetLatestFileName(t *testing.T) {
+	chContainer := setupClickHouseContainer(t)
+
+	// Insert test data
+	conn, err := chContainer.GetClickHouseAsConn()
+	require.NoError(t, err)
+	deviceAddr1 := randAddress()
+	deviceAddr2 := randAddress()
+	ctx := context.Background()
+	insertTestData(t, ctx, conn, nameindexer.Subject{Address: ref(deviceAddr1)}, "file1.json", time.Now().Add(-1*time.Hour))
+	insertTestData(t, ctx, conn, nameindexer.Subject{Address: ref(deviceAddr1)}, "file2.json", time.Now())
+
+	tests := []struct {
+		name          string
+		deviceAddr    common.Address
+		expectedFile  string
+		expectedError bool
+	}{
+		{
+			name:         "valid latest file",
+			deviceAddr:   deviceAddr1,
+			expectedFile: "file2.json",
+		},
+		{
+			name:          "no records",
+			deviceAddr:    deviceAddr2,
+			expectedError: true,
+		},
+	}
+
+	indexFileService := service.New(conn, nil, "test-bucket", dataType)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			filename, err := indexFileService.GetLatestFileName(context.Background(), nameindexer.Subject{
+				Address: &tt.deviceAddr,
+			})
+
+			if tt.expectedError {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tt.expectedFile, filename)
+			}
+		})
+	}
+}
+
+// TestGetDataFromFile tests the GetDataFromFile function.
+func TestGetDataFromFile(t *testing.T) {
+	chContainer := setupClickHouseContainer(t)
+	deviceAddr1 := randAddress()
+	deviceAddr2 := randAddress()
+
+	conn, err := chContainer.GetClickHouseAsConn()
+	require.NoError(t, err)
+	ctx := context.Background()
+	insertTestData(t, ctx, conn, nameindexer.Subject{Address: ref(deviceAddr1)}, "file1.json", time.Now().Add(-1*time.Hour))
+
+	tests := []struct {
+		name            string
+		deviceAddr      common.Address
+		expectedContent []byte
+		expectedError   bool
+	}{
+		{
+			name:            "valid file content",
+			deviceAddr:      deviceAddr1,
+			expectedContent: []byte(`{"vin": "1HGCM82633A123456"}`),
+		},
+		{
+			name:          "no records",
+			deviceAddr:    deviceAddr2,
+			expectedError: true,
+		},
+	}
+
+	ctrl := gomock.NewController(t)
+	mockS3Client := NewMockObjectGetter(ctrl)
+	content := []byte(`{"vin": "1HGCM82633A123456"}`)
+	mockS3Client.EXPECT().GetObjectWithContext(gomock.Any(), gomock.Any(), gomock.Any()).Return(&s3.GetObjectOutput{
+		Body:          io.NopCloser(bytes.NewReader(content)),
+		ContentLength: ref(int64(len(content))),
+	}, nil).AnyTimes()
+
+	indexFileService := service.New(conn, mockS3Client, "test-bucket", dataType)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			content, err := indexFileService.GetLatestData(context.Background(), nameindexer.Subject{
+				Address: &tt.deviceAddr,
+			})
+
+			if tt.expectedError {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tt.expectedContent, content)
+			}
+		})
+	}
+}
+
+// TestStoreFile tests the StoreFile function.
+func TestStoreFile(t *testing.T) {
+	chContainer := setupClickHouseContainer(t)
+	deviceAddr1 := randAddress()
+
+	conn, err := chContainer.GetClickHouseAsConn()
+	require.NoError(t, err)
+	ctx := context.Background()
+
+	ctrl := gomock.NewController(t)
+	mockS3Client := NewMockObjectGetter(ctrl)
+	mockS3Client.EXPECT().PutObjectWithContext(gomock.Any(), gomock.Any(), gomock.Any()).Return(&s3.PutObjectOutput{}, nil).AnyTimes()
+
+	indexFileService := service.New(conn, mockS3Client, "test-bucket", dataType)
+
+	content := []byte(`{"vin": "1HGCM82633A123456"}`)
+	index := nameindexer.Index{
+		Subject:   nameindexer.Subject{Address: &deviceAddr1},
+		DataType:  dataType,
+		Timestamp: time.Now(),
+	}
+
+	err = indexFileService.StoreFile(ctx, &index, content)
+	require.NoError(t, err)
+
+	// Verify the data is stored in ClickHouse
+	filename, err := indexFileService.GetLatestFileName(ctx, nameindexer.Subject{
+		Address: &deviceAddr1,
+	})
+	require.NoError(t, err)
+	expectedFileName, err := nameindexer.EncodeIndex(&index)
+	require.NoError(t, err)
+	require.Equal(t, expectedFileName, filename)
+}
+
+func ref[T any](x T) *T {
+	return &x
+}
+
+func randAddress() common.Address {
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		log.Fatalf("Failed to generate private key: %v", err)
+	}
+	return crypto.PubkeyToAddress(privateKey.PublicKey)
+}
